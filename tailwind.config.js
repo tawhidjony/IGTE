dc9740870176c964d821d8412504900ba25e08d7
Tawhidur Rahman <tawhid@jadroo.com.bd>
2022-06-07 17:04:22 +0600

    block

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    container: {
+      center: true,
+      padding:"1rem"
+    },
     extend: {
       fontFamily: {
         'sans': ['"Poppins"', 'sans-serif', ...defaultTheme.fontFamily.sans],
