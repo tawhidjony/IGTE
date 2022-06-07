1d974ae4259844942f1562e715bbbed7aaabef48
Tawhidur Rahman <tawhid@jadroo.com.bd>
2022-06-07 16:14:54 +0600

    cart

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['"Poppins"', 'sans-serif', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
